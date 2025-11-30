19cad517c61cb6e6f1627fdac4fa32f5670235e6
AlexMohamedAfk <h7mo555@gmail.com>
2025-11-30 17:08:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,23 @@
 function login() {
-  const username = document.getElementById('username').value.trim();
-  const password = document.getElementById('password').value.trim();
-  const message = document.getElementById('message');
+  const username = document.getElementById("username").value.trim();
+  const password = document.getElementById("password").value.trim();
+  const msg = document.getElementById("message");
 
   if (username === "student" && password === "1234") {
-    message.style.color = "green";
-    message.textContent = "تم تسجيل الدخول كطالب!";
-    // لاحقاً نعمل Redirect لصفحة الطالب
-    // window.location.href = "student.html";
+    window.location.href = "student.html";
   } 
-  
   else if (username === "teacher" && password === "1234") {
-    message.style.color = "green";
-    message.textContent = "تم تسجيل الدخول كمُعلّم!";
-    // window.location.href = "teacher.html";
-  } 
-  
-  else if (username === "admin" && password === "1234") {
-    message.style.color = "green";
-    message.textContent = "تم تسجيل الدخول كمدير النظام!";
-    // window.location.href = "admin.html";
-  } 
-  
-  else {
-    message.style.color = "red";
-    message.textContent = "اسم المستخدم أو كلمة السر غير صحيحة.";
+    window.location.href = "teacher.html";
   }
+  else if (username === "admin" && password === "1234") {
+    window.location.href = "admin.html";
+  }
+  else {
+    msg.style.color = "red";
+    msg.textContent = "بيانات غير صحيحة.";
+  }
+}
+
+function logout() {
+  window.location.href = "index.html";
 }
